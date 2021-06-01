bced718e7c818cb1eb9a927e26a8caae1b24a7a1
jibinjose007 <jibin85jose@gmail.com>
2021-06-01 22:23:01 +0530

    Title added

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div className='title'>
+            <h1>PictureGramam</h1>
+            <h2>Your Picture</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    )
+}
+
+export default Title
