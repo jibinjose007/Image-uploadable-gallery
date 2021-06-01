f5fa1e23fc5674c434c8b8eb1049fec76d233ed7
jibinjose007 <jibin85jose@gmail.com>
2021-06-01 23:44:58 +0530

    upload progress and after is added

--- src/hooks/userStoreage.js
+++ src/hooks/userStoreage.js
@@ -5,4 +5,22 @@ const useStorage = (file) =>{
     const [progress, setProgress] = useState(0);
     const [error, setError] = useState(null);
     const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+
+        //refrences
+        const storegeRef = projectStoreage.ref(file.name)
+
+        storegeRef.put(file).on('state_changed',(snap)=>{
+            //figuring progress
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage)
+        },(err)=>{
+            setError(err);
+        }, async ()=>{                  // fire-after fully uploaded 
+            const url = await storegeRef.getDownloadURL();
+            setUrl(url);
+        })
+
+    }, [file])
 }
