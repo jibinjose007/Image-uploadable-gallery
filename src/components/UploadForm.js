18a2942394615e3869edc6ea6efd25e8a99f1caf
jibinjose007 <jibin85jose@gmail.com>
2021-06-01 23:22:21 +0530

    upload form completed

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UploadForm() {
 
+    const [file, setFiles] = useState(null);
+    const [error, setError] = useState(null)
+
+    const types = ['image/png', 'image/jpeg'];
+
     const changeHandler = (e) =>{
         let selected = e.target.files[0];
-        console.log(selected);
+
+        if (selected && types.includes(selected.type)){
+            setFiles(selected);
+            setError('')
+        }else
+            {setFiles(null);
+            setError('Please select (png or jpeg)');
+        }
+        
     }
 
     return (
         <div>
             <form>
                 <input type="file" onChange={changeHandler} />
+                <div className="output">
+                    {error && <div className='error'>{error}</div>}
+                </div>
             </form>
         </div>
     )
